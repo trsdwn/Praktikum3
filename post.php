823dbf2870704f44a7c9e0a8585618edbfd38c0e
trsdwn <167772593+trsdwn@users.noreply.github.com>
2024-06-19 11:47:16 +0700

    Add files via upload

---
+++ post.php
@@ -0,0 +1,10 @@
+<?php
+echo "<center>Email: ".$_POST['email']."</center><br>";
+echo "<center>Nama Lengkap: ".$_POST['nama']."</center><br>";
+echo "<center>Jenis Kelamin: ".$_POST['jeniskelamin']."</center><br>";
+echo "<center>Tanggal Lahir: ".$_POST['tgllahir']."</center><br>";
+echo "<center>No Telepon: ".$_POST['notelepon']."</center><br>";
+echo "<center>Alamat: ".$_POST['alamat']."</center><br>";
+echo "<center>Username: ".$_POST['username']."</center><br>";
+echo "<center>Password: ".$_POST['password']."</center><br>";
+?>
